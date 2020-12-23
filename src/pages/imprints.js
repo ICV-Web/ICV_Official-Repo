1be659abfbef2ebf7ae5d8ee50d8af65842ca343
tariq <tariqmushtaq19@gmail.com>
2020-12-23 20:37:54 +0500

    new push

--- src/pages/imprints.js
+++ src/pages/imprints.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import Layout from '../components/layout'
 import {Container,Row,Col} from "react-bootstrap"
+import { graphql } from "gatsby"
+import { renderRichText } from "gatsby-source-contentful/rich-text"
 
 
-const imprints = () => {
+
+const imprints = ( { data } ) => {
+
+    const {desc} = data.contentfulImprint
+
+    console.log(desc);
 
 
     return (
@@ -14,16 +21,14 @@ const imprints = () => {
                         <Row>
                             <Col sm={12} md={12}>
                                 <div className="box-partner">
-                                    <div class="item">
-                                    <div class="box-image">
-                                        <div class="client-img">
+                                    <div className="item">
+                                    <div className="box-image">
+                                        <div className="client-img">
                                             
                                         </div>
                                     </div>
-                                    <div class="box-info">
-                                        <p>Please note that your use of our website constitutes your unconditional agreement to follow and be bound by these Terms of Use. Everything you see or read on this website is copyrighted unless otherwise noted and may not be used without the written permission of ICV except as provided in these Terms of Use or in the text on the site.</p>
-                                        <p>ICV reserves the right to update or modify these Terms of Use at any time without prior notice. Your use of the ICV website following any such change constitutes your unconditional agreement to follow and be bound by these Terms of Use as amended. For this reason, we encourage you to review these Terms of Use whenever you use this website. Copyrights, trademarks and trade names contained herein are registered or applied for in multiple countries.</p>
-                                        <p>Except for personal, non-commercial internal use, you are prohibited from using any of the materials and documents shown without obtaining ICV’s prior written permission. This prohibition includes, without limitation, copying, distributing, modifying, reproducing in whole or in part, uploading, transmitting, licensing, selling and publishing.</p>
+                                    <div className="box-info">
+                                        {desc && renderRichText(desc)}
                                     </div>
                                     </div>                                    
                                 </div>
@@ -36,6 +41,21 @@ const imprints = () => {
         )
 }
 
+
+export const query = graphql`
+  {
+  contentfulImprint {
+        desc {
+        raw
+        }
+        descImg {
+        fluid {
+            src
+        }
+        }
+    }
+  }
+`
 
 
 export default imprints
