afcd33ccca314104600348d216571f02d981fd8f
AnasQ <anas.qaiser@synapse-consulting.com.pk>
2021-04-14 15:44:54 +0500

    Testing email sending functionality.

--- src/pages/blogNewsLetter.js
+++ src/pages/blogNewsLetter.js
@@ -1,14 +1,38 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import Layout from "../components/layout"
 
 import {Container, Row, Col} from "react-bootstrap"
 import { Link } from "gatsby"
 import Fade from 'react-reveal/Fade';
-const blogNewsLetter = () => {
+import emailjs from 'emailjs-com'
+
+const BlogNewsLetter = () => {
   
-  fetch("https://intelygic.com/development/ignite-fitness/api/web/v1/user/test", {
-      method: "POST",
-    })
+  const sendEmails = () => { 
+
+    var templateParams = {
+    
+};
+
+    // fetch("https://intelygic.com/development/ignite-fitness/api/web/v1/user/test", {
+    //   method: "POST",
+    //   headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    //   //body: encode({ "form-name": "contact v2", ...formState2 }),
+    // })
+    //   //.then(() => navigate("/validation"))
+    //   .catch(error => console.log(error))
+
+    emailjs.send('service_s17rbh6', 'template_fzjx6ge', templateParams, 'user_25Y1NrEQ6qP3oZDPXy4Qt')
+      .then((result) => {
+          window.location.reload()  //This is if you still want the page to reload (since e.preventDefault() cancelled that behavior) 
+      }, (error) => {
+          console.log(error.text);
+      });
+  }
+  
+  useEffect(() => {
+    sendEmails();
+  }, []);
 
     return (
       <Layout>
@@ -29,4 +53,4 @@ const blogNewsLetter = () => {
     )
     }
 
-export default blogNewsLetter
+export default BlogNewsLetter
